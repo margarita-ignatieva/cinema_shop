b43661d4eea9696fb14d5271f46355d0763f4566
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-10-07 09:59:51 +0300

    info to exceptions added

--- src/main/java/com/cinema/shop/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/com/cinema/shop/dao/impl/CinemaHallDaoImpl.java
@@ -26,7 +26,7 @@ public class CinemaHallDaoImpl implements CinemaHallDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert Cinema Hall entity", e);
+            throw new DataProcessingException("Can't insert Cinema Hall" + cinemaHall, e);
         } finally {
             if (session != null) {
                 session.close();
--- src/main/java/com/cinema/shop/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/cinema/shop/dao/impl/MovieDaoImpl.java
@@ -26,7 +26,7 @@ public class MovieDaoImpl implements MovieDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert Movie entity", e);
+            throw new DataProcessingException("Can't insert Movie" + movie, e);
         } finally {
             if (session != null) {
                 session.close();
