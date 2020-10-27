2e3fe08fdc71fd9f29e2d1896ba2161019f13196
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-10-27 16:27:08 +0300

    cinemahall controller added

---
+++ src/main/java/com/cinema/shop/mappers/CinemaHallMapper.java
@@ -0,0 +1,25 @@
+package com.cinema.shop.mappers;
+
+import com.cinema.shop.model.CinemaHall;
+import com.cinema.shop.model.dto.CinemaHallRequestDto;
+import com.cinema.shop.model.dto.CinemaHallResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallMapper {
+
+    public CinemaHall getFromDto(CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(cinemaHallRequestDto.getCapacity());
+        cinemaHall.setDescription(cinemaHallRequestDto.getDescription());
+        return  cinemaHall;
+    }
+
+    public CinemaHallResponseDto getCinemaHallDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto cinemaHallResponseDto = new CinemaHallResponseDto();
+        cinemaHallResponseDto.setId(cinemaHall.getId());
+        cinemaHallResponseDto.setCapacity(cinemaHall.getCapacity());
+        cinemaHallResponseDto.setDescription(cinemaHall.getDescription());
+        return cinemaHallResponseDto;
+    }
+}
---
+++ src/main/java/com/cinema/shop/model/dto/CinemaHallResponseDto.java
@@ -0,0 +1,10 @@
+package com.cinema.shop.model.dto;
+
+import lombok.Data;
+
+@Data
+public class CinemaHallResponseDto {
+    private Long id;
+    private int capacity;
+    private String description;
+}
