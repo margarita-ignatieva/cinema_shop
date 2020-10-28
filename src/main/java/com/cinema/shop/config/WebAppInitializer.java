41341712336fb0a4b2b570201152cb297d6f789f
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-10-29 00:23:57 +0300

    security config

--- src/main/java/com/cinema/shop/config/WebAppInitializer.java
+++ src/main/java/com/cinema/shop/config/WebAppInitializer.java
@@ -6,7 +6,8 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{AppConfig.class};
+        return new Class[]{
+                AppConfig.class, SecurityConfig.class};
     }
 
     @Override
--- src/main/java/com/cinema/shop/mappers/UserMapper.java
+++ src/main/java/com/cinema/shop/mappers/UserMapper.java
@@ -8,13 +8,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserMapper {
 
-    public User getUserFromDto(UserRequestDto userRequestDto) {
-        User user = new User();
-        user.setEmail(userRequestDto.getEmail());
-        user.setPassword(userRequestDto.getPassword());
-        return user;
-    }
-
     public UserResponseDto getUserDto(User user) {
         UserResponseDto userResponseDto = new UserResponseDto();
         userResponseDto.setId(user.getId());
